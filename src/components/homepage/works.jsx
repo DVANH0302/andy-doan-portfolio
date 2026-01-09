5bfc14a6968cf192eb4e4690ab15bc8dee93aa80
Andy <doanvietanh03022003@gmail.com>
2026-01-09 12:51:49 +1100

    add work exp in homepage

--- src/components/homepage/works.jsx
+++ src/components/homepage/works.jsx
@@ -9,7 +9,11 @@ import "./styles/works.css";
 const Works = () => {
 	return (
 		<div className="works">
-			<Link to="/experience" className="work-card-link" aria-label="View details about my work experience">
+			<Link
+				to="/experience"
+				className="work-card-link"
+				aria-label="View details about my work experience"
+			>
 				<Card
 					icon={faBriefcase}
 					title="Work"
@@ -17,15 +21,35 @@ const Works = () => {
 						<div className="works-body">
 							<div className="work">
 								<img
+									src="./digitalnexusai.png"
+									alt="BWS"
+									className="work-image"
+								/>
+								<div className="work-title">
+									Digital Nexus AI
+								</div>
+								<div className="work-subtitle">
+									Full Stack Developer Intern
+								</div>
+								<div className="work-duration">
+									Dec 2025 – Now
+								</div>
+							</div>
+							<div className="work">
+								<img
 									src="/bws.png"
 									alt="BWS"
 									className="work-image"
 								/>
-								<div className="work-title">Business Web Solutions</div>
+								<div className="work-title">
+									Business Web Solutions
+								</div>
 								<div className="work-subtitle">
 									Full Stack Developer Intern
 								</div>
-								<div className="work-duration">Apr 2025 – Aug 2025</div>
+								<div className="work-duration">
+									Apr 2025 – Aug 2025
+								</div>
 							</div>
 
 							<div className="work">
@@ -38,11 +62,15 @@ const Works = () => {
 								<div className="work-subtitle">
 									Solution Developer
 								</div>
-								<div className="work-duration">July 2024 - Feb 2025</div>
+								<div className="work-duration">
+									July 2024 - Feb 2025
+								</div>
 							</div>
 
 							<div className="work-cta">
-								<span className="work-view-btn">View details</span>
+								<span className="work-view-btn">
+									View details
+								</span>
 							</div>
 						</div>
 					}
